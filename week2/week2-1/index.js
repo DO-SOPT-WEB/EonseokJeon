dbda9efee286eba10298b99d33f27130be947d06
Jeon Eonseok <jseokeon@gmail.com>
2023-12-15 15:31:44 +0900

    feat: preview section 화살표 기능 추가

--- week2/week2-1/index.js
+++ week2/week2-1/index.js
@@ -1,6 +1,9 @@
 const topBtn = document.querySelector(".top-btn");
 const description = document.querySelector(".content1__img__text__description");
 const moreText = document.querySelector(".more");
+const heroSection = document.querySelector(".imgs");
+const prevBtn = document.querySelector(".arrow-left");
+const nextBtn = document.querySelector(".arrow-right");
 
 if (description.textContent.length > 60) {
   moreText.style.display = "inline-block";
@@ -12,7 +15,13 @@ moreText.addEventListener("click", () => {
   description.style.height = "auto";
 });
 
+prevBtn.onclick = () => {
+  heroSection.scrollLeft -= 1680;
+};
+nextBtn.onclick = () => {
+  heroSection.scrollLeft += 1680;
+};
+
 document.addEventListener("scroll", () => {
-  console.log(scrollY, window.innerHeight, scrollY / window.innerHeight);
   topBtn.style.opacity = scrollY / window.innerHeight;
 });
