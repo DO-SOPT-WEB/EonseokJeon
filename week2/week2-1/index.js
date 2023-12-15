2dc61955c24db6eaa136447700413be0e0327255
Jeon Eonseok <jseokeon@gmail.com>
2023-12-15 14:08:29 +0900

    feat: 더보기 기능 구현

--- week2/week2-1/index.js
+++ week2/week2-1/index.js
@@ -1,6 +1,18 @@
-const topBtn = document.querySelector('.top-btn');
+const topBtn = document.querySelector(".top-btn");
+const description = document.querySelector(".content1__img__text__description");
+const moreText = document.querySelector(".more");
 
-document.addEventListener('scroll', () => {
+if (description.textContent.length > 60) {
+  moreText.style.display = "inline-block";
+}
+
+moreText.addEventListener("click", () => {
+  moreText.style.display = "none";
+  description.style.display = "inline-block";
+  description.style.height = "auto";
+});
+
+document.addEventListener("scroll", () => {
   console.log(scrollY, window.innerHeight, scrollY / window.innerHeight);
   topBtn.style.opacity = scrollY / window.innerHeight;
-})
+});
